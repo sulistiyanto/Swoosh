01a3856fa4082821a0d3e2f46597288595944c8f
sulistiyanto <sulistiyanto404@gmail.com>
2018-01-27 13:00:41 +0700

    testing doang

--- app/src/main/java/com/tubandev/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/tubandev/swoosh/LeagueActivity.kt
@@ -2,11 +2,13 @@ package com.tubandev.swoosh
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+        Log.d("dsf", "test")
     }
 }
