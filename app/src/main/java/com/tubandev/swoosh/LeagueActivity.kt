0420555044726ca0cb94d01dc36bc173662b20b4
sulistiyanto <sulistiyanto404@gmail.com>
2018-01-27 20:47:43 +0700

    lesson 4

--- app/src/main/java/com/tubandev/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/tubandev/swoosh/LeagueActivity.kt
@@ -1,10 +1,9 @@
 package com.tubandev.swoosh
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
